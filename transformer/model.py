023183087017352c7011ea665773718f73ba773f
Mohammad Nomaan Qureshi <nqafzal@gmail.com>
2024-01-15 02:42:20 -0500

    more progress

--- transformer/model.py
+++ transformer/model.py
@@ -159,4 +159,67 @@ class MultiHeadAttentionBlock(nn.Module):
 
         x = self.W_o(x)
 
-        return x    
+        return x
+    
+
+class ResidualConnection(nn.Module):
+
+    def __init__(self, dropout:float) -> None:
+        super().__init__()
+
+        self.dropout = nn.Dropout(dropout)
+        self.norm = LayerNormalization()
+
+    
+    def forward(self, x, sublayer):
+        return x + self.dropout(sublayer(self.norm(x)))
+    
+
+class EncoderBlock(nn.Module):
+
+    def __init__(self, self_attention_block:MultiHeadAttentionBlock, feed_forward_block:FeedForwardBlock) -> None:
+        super().__init__()
+        self.self_attention_block = self_attention_block
+        self.feed_forward_block = feed_forward_block
+
+        self.residual_connections = nn.ModuleList([ResidualConnection() for _ in range(2)])
+
+    def forward(self, x, src_mask):
+        x = self.residual_connections[0](x, lambda x: self.self_attention_block(x, x, x, src_mask))
+        x = self.residual_connections[1](x, self.feed_forward_block)
+
+
+class Encoder(nn.Module):
+
+    def __init__(self, layers:nn.ModuleList) -> None:
+        super().__init__()
+
+        self.layers = layers 
+        self.norm = LayerNormalization()
+
+    def forward(self, x, src_mask):
+        for layer in self.layers:
+            x = layer(x, src_mask)
+
+        return self.norm(x)
+    
+
+class DecoderBlock(nn.Module):
+    
+    def __init__(self, self_attention_block:MultiHeadAttentionBlock, cross_attention_block:MultiHeadAttentionBlock, feed_forward_block:FeedForwardBlock) -> None:
+        super().__init__()
+
+        self.self_attention_block = self_attention_block
+        self.cross_attention_block = cross_attention_block
+        self.feed_forward_block = feed_forward_block
+
+        self.residual_connections = nn.ModuleList([ResidualConnection() for _ in range(3)])
+
+    def forward(self, x, encoder_output, src_mask, tgt_mask):
+        x = self.residual_connections[0](x, lambda x: self.self_attention_block(x, x, x, tgt_mask))
+        x = self.residual_connections[1](x, lambda x: self.cross_attention_block(x, encoder_output, encoder_output, src_mask))
+        x = self.residual_connections[2](x, self.feed_forward_block)
+
+        return x 
+
+        
